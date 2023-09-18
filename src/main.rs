e57ee8219abed42968a04f5297758f7b3c71c805
BiancaDullius <bianca.dullius@paipe.co>
2023-09-18 15:34:31 -0300

    adicionando movimentação para baixo

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,12 @@ use ggez::graphics::{DrawParam, Mesh};
 use ggez::input::keyboard::KeyCode;
 use ggez::{Context, ContextBuilder, GameResult};
 
+use std::env;
+
+const WINDOW_BOTTOM: f32 = 390.0;
+const WINDOW_Y: f32 = 800.0;
+const WINDOW_H: f32 = 800.0;
+const WINDOW_MIDDLE: f32 = 650.0;
 #[derive(Clone, Copy, PartialEq, Debug)]
 struct Vector2 {
     x: f32,
@@ -23,6 +29,30 @@ struct Rocket {
     weight: f32,
     is_moving: bool,
     fuel: f32,
+    is_flying: bool,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct Platform {
+    shape: Rect,
+}
+impl Platform {
+    fn new(x: f32, y: f32, w: f32, h: f32) -> Platform {
+        return Platform {
+            shape: Rect::new(x, y, w, h),
+        };
+    }
+
+    fn create_mesh(&self, context: &mut Context) -> GameResult<Mesh> {
+        let rect_bounds: Rect = self.shape.clone();
+        let mesh = graphics::Mesh::new_rectangle(
+            context,
+            graphics::DrawMode::fill(),
+            rect_bounds,
+            Color::YELLOW,
+        )?;
+        Ok(mesh)
+    }
 }
 
 impl Vector2 {
@@ -31,29 +61,13 @@ impl Vector2 {
     }
 }
 
-// impl GridPosition {
-//     pub fn new(x: i16, y: i16) -> Self {
-//         GridPosition { x, y }
-//     }
-
-//     pub fn new_from_move(pos: GridPosition, dir: Direction) -> Self {
-//         match dir {
-//             Direction::Up => GridPosition::new(pos.x, (pos.y - 1).modulo(GRID_SIZE.1)),
-//             Direction::Down => GridPosition::new(pos.x, (pos.y + 1).modulo(GRID_SIZE.1)),
-//             Direction::Left => GridPosition::new((pos.x - 1).modulo(GRID_SIZE.0), pos.y),
-//             Direction::Right => GridPosition::new((pos.x + 1).modulo(GRID_SIZE.0), pos.y),
-//         }
-//     }
-// }
-
 impl Rocket {
-    fn new(x: f32, y: f32) -> Rocket {
+    fn new(x: f32, y: f32, weight: f32, fuel: f32) -> Rocket {
         let acceleration = Vector2::new(0.0, 0.0);
         let velocity = Vector2::new(0.0, 0.0);
-        let up_force = Vector2::new(0.0, -0.009);
-        let left_force = Vector2::new(-0.009, 0.0);
-        let right_force = Vector2::new(0.009, 0.0);
-        let weight = 3000.0;
+        let up_force = Vector2::new(0.0, -0.0009);
+        let left_force = Vector2::new(-0.0009, 0.0);
+        let right_force = Vector2::new(0.0009, 0.0);
 
         Rocket {
             shape: Rect::new(x, y, 60.0, 150.0),
@@ -64,7 +78,8 @@ impl Rocket {
             right_force,
             weight,
             is_moving: false,
-            fuel: 100.0,
+            fuel,
+            is_flying: true,
         }
     }
 
@@ -92,77 +107,152 @@ impl Rocket {
         self.shape.y = self.velocity.y;
 
         self.acceleration.x *= 0.0;
-        self.acceleration.y *= 0.0;
+        // self.acceleration.x *= 0.0;
+        // self.acceleration.y *= 0.0;
     }
 
     fn hit_ground(&mut self, window_height: f32) {
         if self.shape.y + self.shape.h >= window_height {
+            println!("Chegou no chao");
             self.velocity.y = 0.0;
             self.shape.y = window_height - self.shape.h;
+            self.is_flying = false;
         }
     }
 
     fn up(&mut self) {
         let up_force = self.up_force.clone();
         self.apply_force(&up_force);
+        self.fuel += &up_force.y * self.weight * 0.01;
     }
 
+    fn down_movimentation(&mut self) {
+        let down_force = Vector2::new(0.0, -0.00009);
+        self.apply_force(&down_force);
+        self.acceleration.y *= 0.0;
+    }
     fn left_movimentation(&mut self) {
         let left_force = self.left_force.clone();
         self.apply_force(&left_force);
+        self.fuel += &left_force.x * self.weight * 0.01;
     }
 
     fn rigth_movimentation(&mut self) {
         let right_force = self.right_force.clone();
         self.apply_force(&right_force);
+        self.fuel -= &right_force.x * self.weight * 0.01;
+    }
+
+    fn is_running_into_platform(&self, platform: &Platform) -> bool {
+        return self.shape.x >= platform.shape.x + platform.shape.w
+            && self.shape.x - self.shape.w
+                <= platform.shape.x + platform.shape.w + platform.shape.w
+            && self.shape.y >= WINDOW_MIDDLE;
+    }
+
+    fn has_fuel(&self) -> bool {
+        return !(self.fuel <= 0.0);
+    }
+
+    fn fall(&mut self, platform_h: f32) {
+        self.shape.y = WINDOW_Y - self.shape.h - platform_h;
+        self.is_flying = false;
     }
 }
-pub struct MyGame {
+struct MyGame {
     rocket: Rocket,
     rocket_mesh: Mesh,
     gravity: Vector2,
+    platform: Platform,
+    platform_mesh: Mesh,
+    game_state: GameState,
+}
+
+enum GameState {
+    Playing,
+    GameOver,
 }
 
 impl MyGame {
-    pub fn new(_context: &mut Context) -> GameResult<MyGame> {
+    pub fn new(
+        _context: &mut Context,
+        rocket: Rocket,
+        gravity: Vector2,
+        platform: Platform,
+    ) -> GameResult<MyGame> {
         // Load/create resources such as images here.
-        let rocket = Rocket::new(0.0, 0.0);
+
         let rocket_mesh = rocket.create_mesh(_context)?;
-        let gravity = Vector2::new(0.0, 0.001);
+        let platform_mesh = platform.create_mesh(_context)?;
+        let game_state = GameState::Playing;
 
         Ok(MyGame {
             rocket,
             rocket_mesh,
             gravity,
+            platform,
+            platform_mesh,
+            game_state,
         })
     }
 }
 
 impl EventHandler for MyGame {
     fn update(&mut self, context: &mut Context) -> GameResult {
-        println!("Is moving: {}", self.rocket.is_moving);
-        self.rocket.apply_force(&self.gravity);
-        self.rocket.fly();
-        self.rocket.hit_ground(800.0);
+        match self.game_state {
+            GameState::Playing => {
+                println!("Has Fuel: {}", self.rocket.has_fuel());
+                if self.rocket.is_flying {
+                    self.rocket.apply_force(&self.gravity);
+                    self.rocket.fuel -= &self.gravity.y * self.rocket.weight * 0.01;
+                    self.rocket.fly();
+                    self.rocket.hit_ground(WINDOW_Y);
+                } else if self.rocket.is_running_into_platform(&self.platform) {
+                    self.gravity = Vector2::new(0.0, 0.0);
+                    self.rocket.fall(20.0);
+                    self.game_state = GameState::GameOver;
+                } else {
+                    self.gravity = Vector2::new(0.0, 0.0);
+                    self.rocket.fall(0.0);
+                    self.game_state = GameState::GameOver;
+                }
 
-        if context.keyboard.is_key_just_released(KeyCode::Up)
-            || context.keyboard.is_key_just_released(KeyCode::Right)
-            || context.keyboard.is_key_just_released(KeyCode::Left)
-        {
-            self.rocket.is_moving = false;
-        }
+                if !self.rocket.has_fuel() && !self.rocket.is_running_into_platform(&self.platform)
+                {
+                    println!("Perdeu o jogo, acabou combustivel");
+                    self.gravity = Vector2::new(0.0, 0.0);
+                    self.rocket.fall(0.0);
+                    self.game_state = GameState::GameOver;
+                }
 
-        if context.keyboard.is_key_pressed(KeyCode::Up) {
-            self.rocket.is_moving = true;
-            self.rocket.up();
-        }
-        if context.keyboard.is_key_pressed(KeyCode::Left) {
-            self.rocket.is_moving = true;
-            self.rocket.left_movimentation();
-        }
-        if context.keyboard.is_key_pressed(KeyCode::Right) {
-            self.rocket.is_moving = true;
-            self.rocket.rigth_movimentation();
+                if context.keyboard.is_key_just_released(KeyCode::Up)
+                    || context.keyboard.is_key_just_released(KeyCode::Right)
+                    || context.keyboard.is_key_just_released(KeyCode::Left)
+                {
+                    self.rocket.is_moving = false;
+                }
+
+                if context.keyboard.is_key_pressed(KeyCode::Up) {
+                    self.rocket.is_moving = true;
+                    self.rocket.up();
+                }
+                if context.keyboard.is_key_pressed(KeyCode::Left) {
+                    self.rocket.is_moving = true;
+                    self.rocket.left_movimentation();
+                }
+                if context.keyboard.is_key_pressed(KeyCode::Right) {
+                    self.rocket.is_moving = true;
+                    self.rocket.rigth_movimentation();
+                }
+
+                if context.keyboard.is_key_pressed(KeyCode::Down) {
+                    self.rocket.is_moving = true;
+                    self.rocket.down_movimentation();
+                }
+            }
+            GameState::GameOver => {
+                println!("Game over");
+            }
         }
 
         Ok(())
@@ -176,21 +266,53 @@ impl EventHandler for MyGame {
             DrawParam::default().dest([self.rocket.shape.x, self.rocket.shape.y]),
         );
 
+        canvas.draw(
+            &self.platform_mesh,
+            DrawParam::default().dest([self.platform.shape.x, self.platform.shape.y]),
+        );
+
         canvas.finish(context)
     }
 }
 
 fn main() {
-    // Make a Context.
-    let window_mode = WindowMode::default().dimensions(750.0, 800.0);
+    let window_mode = WindowMode::default().dimensions(750.0, WINDOW_H);
     let (mut context, event_loop) = ContextBuilder::new("Foguete", "Bianca Beppler")
         .window_mode(window_mode)
         .build()
         .expect("Could not create ggez context!");
-    // graphics::set_window_title(&context, "Jogo do Foguete");
 
-    let my_game = MyGame::new(&mut context).unwrap();
+    let args: Vec<String> = env::args().collect();
 
-    // Run!
+    let weight: f32 = match args[1].parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Error: Invalid weight value");
+            std::process::exit(1);
+        }
+    };
+
+    let fuel: f32 = match args[2].parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Error: Invalid fuel value");
+            std::process::exit(1);
+        }
+    };
+
+    let gravity: f32 = match args[3].parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Error: Invalid gravity value");
+            std::process::exit(1);
+        }
+    };
+
+    let rocket = Rocket::new(0.0, 0.0, weight, fuel);
+    let platform = Platform::new(160.0, WINDOW_BOTTOM, 100.0, 30.0);
+    let gravity = Vector2::new(0.0, gravity);
+
+    let my_game = MyGame::new(&mut context, rocket, gravity, platform).unwrap();
+
     event::run(context, event_loop, my_game);
 }
